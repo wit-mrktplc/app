3c91579a342a97542ad7f0a8f2c7d792b842537e
FroelingDev <alonsofroelingn@wit.edu>
2025-03-15 17:49:22 -0400

    designed the tab bar and made the icons change on focused

--- src/app/account.tsx
+++ src/app/account.tsx
@@ -2,11 +2,20 @@ import { Pressable, SafeAreaView } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { deauthenticate } from "@/store/auth/auth-slice";
-import { useAppDispatch } from "@/hooks/useApp";
+import { useAppDispatch, useAppSelector } from "@/hooks/useApp";
 import tw from "twrnc";
+import { router } from "expo-router";
+import { useEffect } from "react";
 
 export default function AccountScreen() {
   const dispatch = useAppDispatch();
+  const auth = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!auth.authed) {
+      router.replace("/");
+    }
+  }, [auth.authed]);
 
   return (
     <ThemedView style={tw`flex-1 p-4`}>
